910e244ee6f5e26c5a56d9554d8e25982ff75fa9
Wilker Bruno Nogueira <33883256+Wilkerbn@users.noreply.github.com>
2019-09-26 21:11:50 -0300

    Add files via upload

---
+++ Aula06/src/aula06/Aula06.java
@@ -0,0 +1,15 @@
+
+package aula06;
+
+
+public class Aula06 {
+
+    public static void main(String[] args) {
+        ControleRemoto c1 = new ControleRemoto();
+        c1.ligar();
+        c1.maisVolume();
+        c1.ligarMudo();
+        c1.abrirMenu();
+    }
+    
+}
---
+++ Aula07/src/aula07/Controlador.java
@@ -0,0 +1,13 @@
+
+package aula07;
+
+
+public interface Controlador {
+    public abstract void apresentar();
+    public abstract void status(); 
+    public abstract void ganharLuta();
+    public abstract void perderLuta(); 
+    public abstract void empatarLuta();
+        
+    
+}
